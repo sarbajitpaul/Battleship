28a1a22eefc90c85fcd13f9434cc9cbd24c6f878
Sarbajit Paul <sarbajitpaul1996@gmail.com>
2016-11-20 23:10:34 +0530

    TextView Formatting going on

--- app/src/main/java/com/example/pirateer/battleship/Instructions.java
+++ app/src/main/java/com/example/pirateer/battleship/Instructions.java
@@ -2,6 +2,8 @@ package com.example.pirateer.battleship;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class Instructions extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class Instructions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        TextView instructionText = (TextView) findViewById(R.id.instructionText);
+        instructionText.setText(Html.fromHtml(getString(R.string.instructions)));
     }
 }
